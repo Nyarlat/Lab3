47647d5291fe69b9d7972e608d2e5da8fb9f5481
Nyarlat <nyarlat7@gmail.com>
2020-12-07 19:51:06 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+
+using namespace std;
+
+class Ishape {
+public:
+    Ishape() {
+    }
+    virtual void show_parametri() {
+        cout << " Абстрактная фигура " << endl;
+    }
+    ~Ishape() {
+        cout << "Фигура удалилась" << endl;
+    }
+};
+
+class MyStorage {
+private:
+    Ishape** objects;
+    int size;
+public:
+    MyStorage(int size) {
+        this->size = size;
+        objects = new Ishape* [size];
+    }
+
+    ~MyStorage() {
+        delete[] objects;
+    }
+
+    void SetObject(int index, Ishape* object) {
+        objects[index] = object;
+    }
+    void addObject(int amount, Ishape* object) {
+        size = size + amount;
+        objects = new Ishape* [amount];
+    }
+
+    void delObject(int index) {
+        delete[] objects;
+        objects = nullptr;
+    }
+
+    Ishape& GetObject(int index) {
+        return *objects[index];
+    }
+};
+
+
+int main() {
+        setlocale(LC_ALL, "Rus");
+        MyStorage storage(10);
+
+
+
+        system("pause");
+        return 0;
+
+}
