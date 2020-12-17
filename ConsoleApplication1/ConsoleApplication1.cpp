8190615678cb70754b636612cf6ccb1d352ef6d7
Nyarlat <nyarlat7@gmail.com>
2020-12-17 19:16:13 +0500

    Добавил наследников, которые можно хранить в MyStorage

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -4,47 +4,89 @@
 
 using namespace std;
 
-class Ishape {
+class Object {
 public:
-    Ishape() {
+    Object() {
+        cout << "Object()" << endl;
     }
-    virtual void show() {
-        cout << " Абстрактная фигура " << endl;
+
+    ~Object() {
+        cout << "~Object" << endl;
     }
-    ~Ishape() {
-        cout << "Фигура удалилась" << endl;
+
+    virtual void show_value() {
+    }
+};
+
+class  Subject1 : public Object {
+protected: 
+    int x;
+public: 
+    Subject1() {
+        cout << "Subject1()" << endl;
+        x = rand();
+    }
+    
+    ~Subject1() {
+        cout << "~Subject1()" << endl;
+    }
+
+    void show_value() {
+        cout << "x=" << x << endl;
+    }
+};
+
+class  Subject2 : public Object {
+protected:
+    int y;
+public:
+    Subject2() {
+        cout << "Subject2()" << endl;
+        y = rand();
+    }
+
+    ~Subject2() {
+        cout << "~Subject2()" << endl;
+    }
+
+    void show_value() {
+        cout << "y=" << y << endl;
     }
 };
 
 class MyStorage {
 private:
-    Ishape** objects;
+    Object** objects;
     int size;
 public:
     MyStorage(int size) {
         this->size = size;
-        objects = new Ishape* [size];
+        objects = new Object* [size];
     }
 
     ~MyStorage() {
         delete[] objects;
     }
 
-    void SetObject(int index, Ishape* object) {
+    void SetObject(int index, Object* object) {
         objects[index] = object;
     }
-    void addObject(int amount, Ishape* object) {
-        size = size + amount;
-        objects = new Ishape* [amount];
-    }
-
+    /*void addObject(int amount, Object* object) {
+        for (int i = 1; i < amount; ++i) {
+            *objects = (Object*)realloc(*objects, sizeof(Object) * i);
+            //*objects[size+i] = new Ishape*;
+        }
+        //size = size + amount;
+    }*/
+    
     void delObject(int index) {
-        delete[] objects;
-        objects = nullptr;
+        objects[index] = NULL;
+        size = size - 1;
     }
 
-    Ishape& GetObject(int index) {
-        return *objects[index];
+    Object& GetObject(int index) {
+        while (objects[index] != nullptr)
+            return *objects[index];
     }
 
     int GetSize() {
@@ -57,13 +99,22 @@ int main() {
         setlocale(LC_ALL, "Rus");
         MyStorage storage(10);
         // добавляем в него объекты
+        for (int i = 0; i < 10; i++) {
+            int random = rand() % 2;
+            Object* obj;
+            if (random == 1) {
+                Subject1* sub1 = new Subject1();
+                obj = sub1;
+            }
+            else {
+                Subject2* sub2 = new Subject2();
+                obj = sub2;
+            }
+        }
         for (int i = 0; i < storage.GetSize(); i++)
-            storage.SetObject(i, new Ishape());
-        // обращаемся поочередно ко всем
-        for (int i = 0; i < storage.GetSize(); i++)
-            storage.GetObject(i).show();
+            storage.GetObject(i);
 
-
+        
 
         system("pause");
         return 0;
